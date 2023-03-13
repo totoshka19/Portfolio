401a1ec959d1f008218373aff9d01cf2861904b0
Anna Kapitanova <totoshechka19@gmail.com>
2023-03-13 20:26:31 +0300

    Сделала секцию видео.

--- Projects/Marketing/Marketing/js/main.js
+++ Projects/Marketing/Marketing/js/main.js
@@ -1,8 +1,43 @@
+/* Открытие секции VIEW MORE PROJECTS */
+
 function openbox() {
     let display = document.getElementById('box').style.display;
     if(display === 'none') {
-        document.getElementById('box').style.display = 'block';
+        document.getElementById('box').style.display = 'grid';
     } else  {
         document.getElementById('box').style.display = 'none';
     }
 }
+
+/* Запуск и пауза видео */
+
+let videoBtn = document.querySelector('.js-video-btn');
+let video = document.querySelector('.js-video');
+
+videoBtn.onclick = function () {
+    video.play();
+    this.classList.add('hide');
+}
+
+video.onclick = function () {
+    if (this.paused) {
+        this.play();
+    } else {
+        this.pause();
+    }
+}
+
+video.addEventListener('ended', videoHandler, false);
+video.addEventListener('pause', videoHandler, false);
+
+function videoHandler() {
+    videoBtn.classList.remove('hide');
+}
+
+
+
+
+
+
+
+
