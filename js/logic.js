722fbf813e8631c8b2d70f364d8d69088a97c0f2
Anna Kapitanova <totoshechka19@gmail.com>
2023-05-01 22:57:04 +0300

    Добавила секцию About.

--- js/logic.js
+++ js/logic.js
@@ -4,7 +4,7 @@ let menuBtn = document.getElementById("menuBtn");
 menuBtn.addEventListener("click", ()=> {
     document.querySelector("body").classList.toggle("toggle-header");
     menuBtn.classList.toggle("fa-xmark");
-})
+});
 // ---------- Header Toggle End ---------- //
 
 // ================================================================================ //
