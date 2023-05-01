c21ee580d068aaa364356b3e5bf89f7fa5fc759a
Anna Kapitanova <totoshechka19@gmail.com>
2023-05-01 17:22:51 +0300

    Добавила кнопку меню для адаптива.

---
+++ js/logic.js
@@ -0,0 +1,10 @@
+// ---------- Header Toggle Start ---------- //
+let menuBtn = document.getElementById("menuBtn");
+
+menuBtn.addEventListener("click", ()=> {
+    document.querySelector("body").classList.toggle("toggle-header");
+    menuBtn.classList.toggle("fa-xmark");
+})
+// ---------- Header Toggle End ---------- //
+
+// ================================================================================ //
