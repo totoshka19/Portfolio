5e220d02166205a15e173fb8d45a7a579c3170ae
Anna Kapitanova <totoshechka19@gmail.com>
2023-05-31 19:11:22 +0300

    Сделала Активное состояние ссылки при запуске прокрутки

--- js/logic.js
+++ js/logic.js
@@ -6,8 +6,6 @@ menuBtn.addEventListener("click", ()=> {
 });
 // ---------- Header Toggle End ---------- //
 
-// ================================================================================ //
-
 // ---------- Typing Animation Start ---------- //
 let type = new Typed(".typingAnimation", {
     strings: ["Front-end Developer", "Web developer", "site layout"],
@@ -31,11 +29,31 @@ window.addEventListener("scroll", function () {
 });
 // ---------- Skill Bar Animation End ---------- //
 
-// ---------- Display the current year ---------- //
+// ---------- Display Current Year Start ---------- //
 let year = document.getElementById('year');
 function getYear() {
     year.innerHTML = new Date().getFullYear();
 }
 getYear();
+// ---------- Display Current Year End ---------- //
 
+// ---------- Active Link State On Scroll Start ---------- //
+//Get All Nav Links
+let navLinks = document.querySelectorAll('header nav ul li a');
+//Get All Nav Sections
+let section = document.querySelectorAll('section');
 
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY + 20;
+    section.forEach(section => {
+        if (scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('active');
+                }
+            })
+        }
+    })
+})
+// ---------- Active Link State On Scroll End ---------- //
