da12d04e6b0ca5de5863205533bfd4d3040bf2a8
Anna Kapitanova <totoshechka19@gmail.com>
2023-06-01 19:52:56 +0300

    Доделала секцию Choose your car

--- Projects/Lease-cars/Lease-cars/js/main.js
+++ Projects/Lease-cars/Lease-cars/js/main.js
@@ -1,3 +1,29 @@
+// ----------------- tabs -----------------
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function (element) {
+    element.addEventListener('click', open);
+});
+
+function open(event) {
+    const tabTarget = event.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach(function (item) {
+        item.classList.remove('tabs__btn-item--active');
+    });
+
+    tabTarget.classList.add('tabs__btn-item--active');
+
+    tabContent.forEach(function (item) {
+        item.classList.remove('tabs__content-item--active');
+    });
+
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
+
+// ----------------- swiper -----------------
 const swiper = new Swiper(".swiper", {
     pagination: {
         el: ".swiper-pagination",
@@ -9,3 +35,4 @@ const swiper = new Swiper(".swiper", {
     // effect: 'fade',
     loop: true,
 });
+
