f44fe198facc26a163d818181ed1f1218d5f022f
Anna Kapitanova <totoshechka19@gmail.com>
2023-06-20 20:11:07 +0300

    сделала вывод текущей даты и времени

--- Projects/Weather/scripts/modules/widgetService.js
+++ Projects/Weather/scripts/modules/widgetService.js
@@ -1,6 +1,16 @@
+import {
+    renderWidgetForecast,
+    renderWidgetOther,
+    renderWidgetToday
+} from "./render.js";
+
 export const startWidget = () => {
     const widget = document.createElement('div');
     widget.classList.add('widget');
+
+    renderWidgetToday(widget)
+    renderWidgetOther(widget)
+    renderWidgetForecast(widget)
 
     return widget;
 };
--- Projects/Weather/scripts/widget.js
+++ Projects/Weather/scripts/widget.js
@@ -1,8 +1,8 @@
-import {startWidget} from "./modules/widgetService";
+import {startWidget} from "./modules/widgetService.js";
 
-const init = (app) => {
+const initWidget = (app) => {
     const widget = startWidget();
     app.append(widget);
 }
 
-init(document.querySelector("#app"));
+initWidget(document.querySelector("#app"));
