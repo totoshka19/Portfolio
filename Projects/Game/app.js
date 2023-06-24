17d00a69d0d56325e70d4986fba266788a2ef182
Anna Kapitanova <totoshechka19@gmail.com>
2023-06-24 13:35:22 +0300

    Сделала мини-игру

---
+++ Projects/Game/app.js
@@ -0,0 +1,32 @@
+const board = document.querySelector('#board');
+const colors = ['#A605C8', '#792D89', '#5D0171', '#D75DF1', '#E3A3F1'];
+const SQUARES_NUMBER = 400;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mousemove', () =>
+    setColor(square));
+
+    square.addEventListener('mouseleave', () =>
+    removeColor(square));
+
+    board.append(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d';
+    element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
